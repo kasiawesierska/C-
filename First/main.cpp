d2329c290a5af058fdfb031fa9f78729f9287851
kasiawesierska <kasiamlynska@op.pl>
2019-10-09 16:34:14 +0200

    First C++ project

---
+++ First/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int x;
+    cout<<"Podaj liczbe"<< endl;
+    cin>> x;
+    cout<< "Twoja liczba to: " << x <<endl;
+    return 0;
+}
