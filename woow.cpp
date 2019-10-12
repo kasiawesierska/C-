649315d93eb34633666fa0ee6e4b78c67983abf3
kasiawesierska <kasiamlynska@op.pl>
2019-10-13 00:14:20 +0200

    new projekt

---
+++ woow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cin >> n;
+    cout << "W";
+    for(i = 1; i <= n; i++)
+        {
+        cout << "o", i;
+        }
+    cout << "w";
+ return 0;
+}
