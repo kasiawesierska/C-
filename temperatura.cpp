bc0a9b671212593955c6f3269a5c6217658ee2c7
kasiawesierska <kasiamlynska@op.pl>
2019-10-13 00:16:24 +0200

    temperatura

---
+++ temperatura.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Celcjuesz na Farenhity" << endl;
+    float deg;
+    cin >> deg;
+    cout << ((deg*1.8) + 32.0) <<endl;
+    return 0;
+
+}
